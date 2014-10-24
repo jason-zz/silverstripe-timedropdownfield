60c6fe3a20923ac41e19bc510da630673f623665
jason-zz <jason.zhang@internetrix.com.au>
2014-10-24 14:24:46 +1100

    tidy CMS css style. add time icon for dropdown toggle.

--- javascript/TimeDropdownField.js
+++ javascript/TimeDropdownField.js
@@ -1,7 +1,23 @@
 (function($) {
 	$('.timedropdown .presets-trigger').entwine({
 		onclick: function(e) {
-			this.parent().find('select.presets').click();
+			this.toggleClass("opened");
+			
+			this.parent().find('select.presets').toggleClass("opened").click().mousedown();
+			
+			// Open Select
+			var element = this.parent().find('select.presets')[0], worked = false;
+			if (document.createEvent) { // all browsers
+			    var e = document.createEvent("MouseEvents");
+			    e.initMouseEvent("mousedown", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+			    worked = element.dispatchEvent(e);
+			} else if (element.fireEvent) { // ie
+			    worked = element.fireEvent("onmousedown");
+			}
+			if (!worked) {
+			}
+			
+			return false;
 		}
 	});
 	
@@ -11,6 +27,9 @@
 			if(this.val()) this.parent().find(':input').val(this.val());
 			// Reset preset value afterwards
 			this.val('');
+			
+			this.removeClass("opened");
+			this.parent().find('.presets-trigger').removeClass("opened");
 		}
 	});
 }(jQuery));
